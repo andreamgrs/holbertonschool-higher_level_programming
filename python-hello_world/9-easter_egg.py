f04647be3c9e1cf4b8c91d641200845d89ea99d3
andreamgrs <andrea.munozgr@gmail.com>
2025-09-03 17:10:01 +1000

    Adding script 9

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-str = "The Zen of Python, by Tim Peters\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\n"
-print(str)
+import this
