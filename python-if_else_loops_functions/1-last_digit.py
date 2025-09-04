aeeab4b892f86884a0849232fe1c0f15dbc9f444
andreamgrs <andrea.munozgr@gmail.com>
2025-09-04 17:04:12 +1000

    Adding task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastnumber = int(str(number)[-1])
+lastnumber = number % 10
+
 if number < 0:
     lastnumber = -lastnumber
 if lastnumber > 5:
