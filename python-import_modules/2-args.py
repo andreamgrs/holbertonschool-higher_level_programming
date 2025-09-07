93a03df694af702b8ef52eb5cfde426912f6f4b8
andreamgrs <andrea.munozgr@gmail.com>
2025-09-07 23:34:14 +1000

    Adding task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if num_args == 0:
         print("{} arguments.".format(num_args))
     elif num_args == 1:
-        print("{} argument".format(num_args))
+        print("{} argument:".format(num_args))
         print("{}: {}".format(num_args, sys.argv[1]))
     else:
-        print("{} argument".format(num_args))
+        print("{} argument:".format(num_args))
         for num in range(1, num_args + 1):
             print("{}: {}".format(num, sys.argv[num]))
