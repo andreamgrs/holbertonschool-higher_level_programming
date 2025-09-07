1bef76d011e522c17166926bb9a91f0ed913b6df
andreamgrs <andrea.munozgr@gmail.com>
2025-09-07 21:13:51 +1000

    Adding task 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        lastnum = number % 10
+    else:
+        lastnum = number % -10
+        lastnum = -lastnum
+    print("{}".format(lastnum), end="")
+    return lastnum
