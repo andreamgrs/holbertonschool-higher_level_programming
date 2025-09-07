3c529fcf8cc1a972cdf05c1466de0c0c0d34bb8c
andreamgrs <andrea.munozgr@gmail.com>
2025-09-07 20:39:13 +1000

    Adding task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if 'a' <= c <= 'z':
+            result = result + chr(ord(c) - 32)
+        else:
+            result = result + c
+    print("{}".format(result))
