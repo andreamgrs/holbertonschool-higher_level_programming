a5cb088a3a62214f31cfccd14c26218af9c478c1
andreamgrs <andrea.munozgr@gmail.com>
2025-09-09 20:39:43 +1000

    Adding task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     len_s = len(sentence)
     if len_s == 0:
-        return ("None")
+        return (len_s, "None")
     else:
         return (len_s, sentence[0])
