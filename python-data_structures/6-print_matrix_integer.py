c106a95831a0b6254b456631a81c1077fe88e9ea
andreamgrs <andrea.munozgr@gmail.com>
2025-09-09 18:48:29 +1000

    Adding task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row_cont in range(len(matrix)):
         for column_cont in range(num_col):
             if column_cont == num_col - 1:
-                print("{:d}". format(matrix[row_cont][column_cont]), end="")
+                print("{:d}".format(matrix[row_cont][column_cont]), end="")
             else:
-                print("{:d} ". format(matrix[row_cont][column_cont]), end="")
+                print("{:d} ".format(matrix[row_cont][column_cont]), end="")
         print()
