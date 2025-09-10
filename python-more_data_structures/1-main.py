b78e50d427af05f522d765d2dcd5e747de8c5365
andreamgrs <andrea.munozgr@gmail.com>
2025-09-10 23:48:57 +1000

    Adding task 1

--- python-more_data_structures/1-main.py
+++ python-more_data_structures/1-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 search_replace = __import__('1-search_replace').search_replace
 
-my_list = [1, 2, 3]
+my_list = [1, 1, 3]
 new_list = search_replace(my_list, 1, 0)
 
 print(new_list)
--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,9 @@ def search_replace(my_list, search, replace):
     if search > len(my_list):
         return my_list
     new_list = my_list.copy()
-    my_index = new_list.index(search)
-    del new_list[my_index]
-    new_list.insert(my_index, replace)
+    for num in new_list:
+        if num == search:
+            my_index = new_list.index(search)
+            del new_list[my_index]
+            new_list.insert(my_index, replace)
     return new_list
