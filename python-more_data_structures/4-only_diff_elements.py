eab660e4f745bb948393f588b09d52a9ab325e38
andreamgrs <andrea.munozgr@gmail.com>
2025-09-11 00:39:00 +1000

    Adding task 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = []
+    if len(set_1) == 0:
+        return set_2
+    if len(set_2) == 0:
+        return set_1
     for element1 in set_1:
         if element1 not in set_2:
             new_set.append(element1)
