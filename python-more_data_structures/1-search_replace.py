86c2d57eb41b498ee31178daa5f2d4480f39d36f
andreamgrs <andrea.munozgr@gmail.com>
2025-09-10 23:42:46 +1000

    Adding task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if search > len(my_list):
+        return my_list
     new_list = my_list.copy()
     my_index = new_list.index(search)
     del new_list[my_index]
