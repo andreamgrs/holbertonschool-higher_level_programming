363df8aa6b549cde5b4bff54fa2d629e408c3ed2
andreamgrs <andrea.munozgr@gmail.com>
2025-09-11 00:25:19 +1000

    Adding task 3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    new_set = []
     for sets in set_1:
         for sets_2 in set_2:
             if sets == sets_2:
-                return sets
+                new_set.append(sets)
+    return new_set
