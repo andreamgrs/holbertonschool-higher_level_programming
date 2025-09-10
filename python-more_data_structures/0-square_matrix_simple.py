c073d9b8ae5489b8f09a75790e91f6a19e4a4dc5
andreamgrs <andrea.munozgr@gmail.com>
2025-09-10 17:38:11 +1000

    Adding new dir

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square = list(map(lambda row: list(map(lambda num: num**2, row)), matrix))
-    return square
+    final_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(my_pow(num))
+        final_matrix.append(new_row)
+    return final_matrix
+
+
+def my_pow(num):
+    return num ** 2
