865c05b863f3a12c01a81c85b5dc7f31c7a0fd09
andreamgrs <andrea.munozgr@gmail.com>
2025-09-14 15:47:29 +1000

    Adding task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    cont = 0
+    while cont < x:
+        try:
+            print("{:d}".format(my_list[cont]), end="")
+        except IndexError:
+            pass
+        else:
+            cont = cont + 1
+    print()
+    return cont
