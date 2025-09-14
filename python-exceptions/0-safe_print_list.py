3015372e862de4f22f111c251bd1b33470b205cc
andreamgrs <andrea.munozgr@gmail.com>
2025-09-14 16:13:00 +1000

    Adding task 2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     cont = 0
-    while cont < x:
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[cont]), end="")
+            print("{:d}".format(my_list[i]), end="")
             cont = cont + 1
-        except:
+        except (TypeError, ValueError):
+            pass
+        except IndexError:
             break
     print()
     return cont
