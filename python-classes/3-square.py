c98a99016ba703f746aec40c46ef99920c38365c
andreamgrs <andrea.munozgr@gmail.com>
2025-09-17 17:37:14 +1000

    Adding task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Class for square
+
+    Attributes:
+        __size(int): The size of the square (private).
+    """
+    def __init__(self, size=0):
+        """Initializes a Square instance.
+
+        Args:
+            size(int): size of the square.
+        Raises:
+                TypeError: if size is not an integer.
+                ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Class methods for get the area of a square.
+
+        Returns:
+            Area of the square.
+        """
+        return self.__size * self.__size
