6589e08d61dd7f1e0775252e1a26d0f3613e8fed
andreamgrs <andrea.munozgr@gmail.com>
2025-09-17 17:26:36 +1000

    Adding task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,15 +14,13 @@ class Square:
         """Initializes a Square instance.
 
         Args:
-            size(int): size of the square
-        """
-        self.__size = size
-
-        """Raises:
+            size(int): size of the square.
+        Raises:
                 TypeError: if size is not an integer.
                 ValueError: if size is less than 0
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
