45e69567fa3dc8c1469c2c1e4b1686e7393c2d58
andreamgrs <andrea.munozgr@gmail.com>
2025-09-23 15:47:02 +1000

    Adding task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -24,11 +24,12 @@ class BaseGeometry:
                 TypeError: if value is not ant integer.
                 ValueError: if value is less or equal to 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
+
+
 class Rectangle(BaseGeometry):
     """Class for rectangle that inherits from BaseGeometry
     """
