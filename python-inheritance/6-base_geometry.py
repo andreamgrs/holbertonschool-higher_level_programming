6a743a0247798fdebd8fc5257434a15fc6a7a70f
andreamgrs <andrea.munozgr@gmail.com>
2025-09-23 14:33:43 +1000

    Adding task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """Class for BaseGeometry
+    """
+
+    def area(self):
+        """Public instance method that raises
+            an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
