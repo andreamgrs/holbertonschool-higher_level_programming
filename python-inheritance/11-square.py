0a50e599003474c4793c0e4b20f7eb7d28928ed1
andreamgrs <andrea.munozgr@gmail.com>
2025-09-24 16:30:34 +1000

    Adding task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Class for Square that inherits from BaseGeometry
+    """
+    def __init__(self, size):
+        """Initializes square instance.
+
+        Args:
+            size(int): size of the square (private).
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+
+    def area(self):
+        """Class method for get the area of a square.
+        Returns:
+            Area of square.
+        """
+        return super().area()
+    
+
+    def __str__(self):
+        """Special method for print string
+        Returns:
+            Returns a string.
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
