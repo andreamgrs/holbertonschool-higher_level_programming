d0b8fbe89e214ff8f6f3ab2f95ecac0834194f0e
andreamgrs <andrea.munozgr@gmail.com>
2025-09-28 18:06:57 +1000

    Adding task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+
+    def append(self, my_item):
+        super().append(my_item)
+        print("Added [{}] to the list.".format(my_item))
+
+    def extend(self, my_item):
+        super().extend(my_item)
+        print("Extended the list with [{}] items".format(len(my_item)))
+
+    def remove(self, my_item):
+        print("Removed [{}] from the list.".format(my_item))
+        super().remove(my_item)
+
+    def pop(self, my_item=None):
+        if (my_item is None):
+            item = super().pop()
+            print("Popped [{}] from the list.".format(item))
+            return item
+        else:
+            print("Popped [{}] from the list.".format(my_item))
+            super().pop(my_item)
