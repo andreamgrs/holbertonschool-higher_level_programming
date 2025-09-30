78f659fff21c436e70a7ee90e20bbbb6f7fc8479
andreamgrs <andrea.munozgr@gmail.com>
2025-09-30 14:01:06 +1000

    Adding new dir and task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Module for read_file function
 """
+
+
 def read_file(filename=""):
     """Function that reads from a file
 
         Args:
             filename: filename
     """
-    with open(filename, encoding="utf-8") as my_file:
+    with open(filename, "r", encoding="utf-8") as my_file:
         print(my_file.read(), end="")
