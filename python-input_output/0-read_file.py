5bf525c0a8bdd836a68ccb5756d204f33c98c008
andreamgrs <andrea.munozgr@gmail.com>
2025-09-30 13:58:31 +1000

    Adding new dir and task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module for read_file function
+"""
 def read_file(filename=""):
+    """Public function to read a text file UTF8 
+        """
     with open(filename, encoding="utf-8") as my_file:
         print(my_file.read())
