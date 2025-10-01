4deb5a9f0c805084de2a89c8ca22570fcc404052
andreamgrs <andrea.munozgr@gmail.com>
2025-10-01 23:57:06 +1000

    Adding task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -44,7 +44,7 @@ class Student:
             Key will be the public attribute name.
             Value will be the value of the public attribute.
         """
-        if json is not None:
+        if len(json) != 0:
             self.first_name = json["first_name"]
             self.last_name = json["last_name"]
             self.age = json["age"]
