8d676e3dd4c8c85b31fcbf2cb8cf70cfc923ba6d
andreamgrs <andrea.munozgr@gmail.com>
2025-10-06 23:11:05 +1100

    Adding new dir and task 1

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import requests
+import csv
+
+
+response = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+
+def fetch_and_print_posts():
+    print("Status code: {}".format(response.status_code))  
+
+    if response.status_code == 200:
+        data = response.json()
+
+        for post in data:
+            print(post['title'])
+    else:
+        print("Fail")
+
+
+def fetch_and_save_posts():
+
+    print("Status code: {}".format(response.status_code))  
+
+    if response.status_code == 200:
+        data = response.json()
+        struct_data = []
+        for post in data:
+                dict_posts = {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            }
+        struct_data.append(dict_posts)
+
+        with open("posts.csv", mode="w", newline='', encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(struct_data)
+    else:
+        print("Fail")
