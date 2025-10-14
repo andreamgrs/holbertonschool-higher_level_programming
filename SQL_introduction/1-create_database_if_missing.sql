3edd571fc10c50439d1dbff598d86a260a435a22
andreamgrs <andrea.munozgr@gmail.com>
2025-10-14 14:51:05 +1100

    Adding task 1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- Creates a database hbtn_0c_0
+-- Creates a database hbtn_0c_0, if the database does not exist should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Deletes a database hbtn_0c_0, if the database does not exist should not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
