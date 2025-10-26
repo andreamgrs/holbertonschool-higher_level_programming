75bd8c2c624ce6557df7cdd6a80c6b55a36a97ca
andreamgrs <andrea.munozgr@gmail.com>
2025-10-26 19:33:17 +1100

    Adding task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+# To prevent not running from import just run this by calling it directly
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()  # To execute query
+    cursor.execute("SELECT * \
+    FROM states WHERE BINARY name LIKE %s \
+    ORDER BY id ASC;", (sys.argv[4],))  # Add query
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
