9ec6efb0a96b481818ea1631131c6e8217d396ef
andreamgrs <andrea.munozgr@gmail.com>
2025-10-27 12:39:51 +1100

    Adding task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,10 +17,10 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        state = session.query(State).order_by(State.id).first()
-        if state:
-            print("{}: {}".format(state.id, state.name))
-    except OperationalError as e:
+    
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
         print("Nothing")
     session.close()
