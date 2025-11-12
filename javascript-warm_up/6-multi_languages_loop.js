18c354960efb27092fee04494cd8da1e4c615714
andreamgrs <andrea.munozgr@gmail.com>
2025-11-12 13:19:56 +1100

    Adding new dir ans task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myVar = [];
+myVar[0] = 'C is fun';
+myVar[1] = 'Python is cool';
+myVar[2] = 'JavaScript is amazing';
+
+let cont = 0;
+while (cont < 3) {
+  console.log(myVar[cont]);
+  cont = cont + 1;
+}
