7d3eea1e0cb44855ac0c22ee2612c8366027ee9c
andreamgrs <andrea.munozgr@gmail.com>
2025-11-12 14:29:28 +1100

    Adding new dir ans task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const argv = process.argv[2];
+const num = parseInt(argv, 10);
+
+function factorial (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+
+console.log(factorial(num));
