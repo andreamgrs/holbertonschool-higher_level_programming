b3196864d07e3d5f822d5e05d457fa28bf8385bb
andreamgrs <andrea.munozgr@gmail.com>
2025-11-16 18:26:34 +1100

    Adding task 0

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -3,15 +3,20 @@ from os.path import exists
 def generate_invitations(template, attendees):
     if not isinstance(template, str):
         print("template must be a string and not {type(template).__name__}")
+        return
     if not template:
         print("Template is empty, no output files generated.")
+        return
     
     if not isinstance(attendees, list):
         print("attendees must be a list of dictionaries and not {type(attendees).__name__}")
+        return
     if len(attendees) < 0:
         print("No data provided, no output files generated.")
+        return
     if not attendees:
         print("Attendees cannot be empty")
+        return
     
     for elem, attendee in enumerate(attendees, start=1):
         text = template
